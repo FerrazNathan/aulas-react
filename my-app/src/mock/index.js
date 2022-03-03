24586eb3fa7019bc7c1ef080faa2a0efad8d2543
Nathan Ferraz <nathanferraz@hotmail.com>
2022-03-03 14:01:41 -0300

    criando dados mocados e aprendendo o map

---
+++ my-app/src/mock/index.js
@@ -0,0 +1,42 @@
+export const familia = [
+  {
+    nome: 'Nathan Ferraz',
+    idade: 34,
+    telefone: 11941124111,
+    data_nasc: '19 / 08 / 1987',
+    mae: 'Rosa Maria Dias Ferraz',
+    pai: ''
+  },
+  {
+    nome: 'Mariane Ferraz',
+    idade: 29,
+    telefone: 11992747681,
+    data_nasc: '16 / 05 / 1992',
+    mae: 'Claudia Santos de Jesus Menezes',
+    pai: 'José Reinaldo',
+  },
+  {
+    nome: 'Filipe Dias Ferraz',
+    idade: 42,
+    telefone: 11974321972,
+    data_nasc: '12 / 10 / 1979',
+    mae: 'Rosa Maria Dias Ferraz',
+    pai: ''
+  },
+  {
+    nome: 'Fernanda Ferraz',
+    idade: 38,
+    telefone: 11996810837,
+    data_nasc: '27 / 03 / 1983',
+    mae: 'Rosa Maria Dias Ferraz',
+    pai: 'Adilson Nascimento'
+  },
+  {
+    nome: 'Roberta Ferraz',
+    idade: 40,
+    telefone: 4784453252,
+    data_nasc: '25 / 01 / 1982',
+    mae: 'Rosa Maria Dias Ferraz',
+    pai: 'Adilson Nascimento'
+  }
+]

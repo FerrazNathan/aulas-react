670ff579dc665896508dc5db9fb99252c593ee8e
Nathan Ferraz <nathanferraz@hotmail.com>
2022-03-10 18:24:38 -0300

    finalizando a tarefa e add parametros ao json

--- my-app/src/mock/index.js
+++ my-app/src/mock/index.js
@@ -4,7 +4,8 @@ export const familia = [
     title: 'Mariane Ferraz',
     name: 'Mariane Santos Silva Ferraz',
     age: 29,
-    date_born: '16 / 05 / 1992',
+    dateBorn: '16 / 05 / 1992',
+    fone: '(11) 9 9274-7681',
     nameMother: 'Claudia Santos de Jesus Menezes',
     nameFather: 'José Reinaldo',
   },
@@ -13,7 +14,8 @@ export const familia = [
     title:'Nicoli Ferraz',
     name: 'Nicoli de Assis Ferraz',
     age: 14,
-    date_born: '10 / 11 / 2007',
+    dateBorn: '10 / 11 / 2007',
+    fone: '(11) 9 7012-3867',
     nameMother: 'Renata de Assis Amaral',
     nameFather: 'Nathan Henrique Dias Ferraz'
   },
@@ -22,7 +24,8 @@ export const familia = [
     title:'Lívia Ferraz',
     name: 'Lívia da Silva Ferraz',
     age: 5,
-    date_born: '07 / 04 / 2016',
+    dateBorn: '07 / 04 / 2016',
+    fone: '(11) 9 4112-4111',
     nameMother: 'Mariane Santos Silva Ferraz',
     nameFather: 'Nathan Henrique Dias Ferraz'
   },
@@ -31,26 +34,29 @@ export const familia = [
     title:'Isabele Ferraz',
     name: 'Isabale da Silva Ferraz',
     age: 4,
-    date_born: '12 /09 / 2018',
+    dateBorn: '12 /09 / 2018',
+    fone: '(11) 9 9274-7681',
     nameMother: 'Mariane Santos Silva Ferraz',
     nameFather: 'Nathan Henrique Dias Ferraz'
-  },
-  {
-    image: './nathan.jpg',
-    title: 'Nathan Ferraz',
-    name: 'Nathan Henrique Dias Ferraz',
-    age: 34,
-    date_born: '19 / 08 / 1987',
-    nameMother: 'Rosa Maria Dias Ferraz',
-    nameFather: ''
   },
   {
     image: './lolo.jpeg',
     title:'Lorrayne Silva',
     name: 'Lorrayne Santos Silva',
     age: 10,
-    date_born: '24 / 04 / 2011',
+    dateBorn: '24 / 04 / 2011',
+    fone: '(11) 9 4112-4111',
     nameMother: 'Mariane Santos Silva Ferraz',
     nameFather: ''
   },
+  {
+    image: './nathan.jpg',
+    title: 'Nathan Ferraz',
+    name: 'Nathan Henrique Dias Ferraz',
+    age: 34,
+    dateBorn: '19 / 08 / 1987',
+    fone: '(11) 9 4112-4111',
+    nameMother: 'Rosa Maria Dias Ferraz',
+    nameFather: ''
+  }
 ]

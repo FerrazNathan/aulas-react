e4d34824ea705b480d748c8436e938fa7707dadf
Nathan Ferraz <nathanferraz@hotmail.com>
2022-04-01 08:50:11 -0300

    limpeza de codigo

--- my-app/src/components/Link/index.jsx
+++ my-app/src/components/Link/index.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import * as S from './styles'
 
-
-
 export default function Link({ active }) {
   return (
     <>
       {
         active &&
-        <S.TextLink onClick={(e) => {window.location.href = 'https://google.com'}}>
-          Link 
+        <S.TextLink onClick={(e) => { window.location.href = 'https://google.com' }}>
+          Link
         </S.TextLink>
       }
     </>

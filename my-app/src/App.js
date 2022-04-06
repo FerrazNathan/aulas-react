8e3c738d1979f1eb6a2d2c87009419259de56994
Nathan Ferraz <nathanferraz@hotmail.com>
2022-04-06 09:37:19 -0300

    finalizando a exibiçao do exercicio

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import './App.css';
-import Card from './components/Card';
-import { dataCard } from './common/constants/dataCard';
-import Link from './components/Link';
+import { Testting, Trainning } from './utils/index';
 
 function App() {
 
   return (
     <>
-      <div className='App-Card container flex'>
-        <Card text={dataCard.TEAMONE} />
-        <Card text={dataCard.TEAMTWO} />
-        <Card text={dataCard.TEAMTREE} />
-        <Card text={dataCard.TEAMFOUR} />
-      </div>
-      <div className='App-Card container flex'>
-        <Link active={true} />
-        <Link active={false} />
-      </div>
+      <form>
+        <input
+          type='number'
+          onChange={(event) => { Trainning(event.target.value, 'é muito louco') }}
+        />
+        <button onClick={(event) => { Testting('Palmeiras', 'Mundial') }}>Buscar</button>
+      </form>
     </>
   );
 }
---
+++ my-app/src/utils/index.jsx
@@ -0,0 +1,7 @@
+export const Trainning = (value, text) => {
+  console.log(value, text);
+}
+
+export const Testting = (team, text) => {
+  alert(`O ${team} não tem ${text}`)
+}

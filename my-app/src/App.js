2b965ff7f66694ef283422e894b4c4da62188831
Nathan Ferraz <nathanferraz@hotmail.com>
2022-04-13 20:03:36 -0300

    finalizando os exercícios

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import './App.css';
-import { Testing, Trainning } from './utils/index';
+import { Testing, Trainning, Open } from './utils/index';
 
 function App() {
 
-  const not = 'Palmeiras'
-  const world = 'Mundial'
-
   return (
-    <>
-      <form className='container'>
-        <h1>Exercício de funções</h1>
-        <input
-          type='search'
-          placeholder='Digite aqui a sua pesquisa'
-          onChange={(event) => { Trainning(event.target.value, 'é muito louco') }}
-        />
-        <button onClick={() => { Testing(not, world) }}>Buscar</button>
-      </form>
-    </>
+    <form className='container'>
+      <h1>Exercício de if else</h1>
+      <input
+        type='search'
+        placeholder='Busque um instrumento'
+        onChange={(event) => { Trainning(event.target.value) }}
+      />
+      <button onClick={() => { Open() }}>Buscar</button>
+      <h1>Exercício de Switch case</h1>
+      <input
+        type='search'
+        placeholder='Faça a sua combinação'
+        onChange={(event) => { Testing(event.target.value) }}
+      />
+      <button onClick={() => { Open() }}>Buscar</button>
+    </form>
   );
 }
 
--- my-app/src/utils/index.jsx
+++ my-app/src/utils/index.jsx
@@ -1,7 +1,29 @@
-export const Trainning = (value, text) => {
-  console.log(value, text);
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-expressions */
+export const Trainning = (text) => {
+  if(text === 'Guitarra'){
+    console.log('Tem cordas')
+  }
+  else if(text === 'Bateria'){
+    console.log('Tem pedal')
+  }
+  else if(text === 'Microfone'){
+    console.log('Tem fios');
+  }
+  else{
+    console.log('Não existe esse instrumento');
+  }
 }
 
-export const Testing = (team, text) => {
-  alert(`O ${team} não tem ${text}`)
+export const Testing = (value) => {
+  switch(value){
+    case 'Frango': console.log('Frango combina com Catupiry');; break;
+    case 'Presunto': console.log('Presunto combina com queijo');; break;
+    case 'Calabresa': console.log('Calabresa combina com cebola');; break;
+    default: console.log('Não combina com nada');
+  }
+}
+
+export const Open = () => {
+  alert('Abra o Console do seu navegador')
 }
